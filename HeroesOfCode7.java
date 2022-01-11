40c9cfd8869ab44d91005afa8612c997519769c3
RalitsaGoranova <97480928+RalitsaGoranova@users.noreply.github.com>
2022-01-11 15:54:06 +0200

    Add files via upload

--- HeroesOfCode7.java
+++ HeroesOfCode7.java
@@ -9,93 +9,106 @@ public class HeroesOfCode7 {
         Map<String , List<Integer>>heroes=new TreeMap<>();
         for(int i=1;i<=n;i++)
         {
-            String[] in=scanner.nextLine().split("\\s");
-            List<Integer> nz=new ArrayList<>();
-            String name=in[0];
-            int hp=Integer.parseInt(in[1]);
-            int mp=Integer.parseInt(in[2]);
+            String[] input=scanner.nextLine().split("\\s");
+            List<Integer> data=new ArrayList<>();
+            String name=input[0];
+            int hp=Integer.parseInt(input[1]);
+            int mp=Integer.parseInt(input[2]);
             if(hp>100||mp>200)
             {
                 return;
             }
-            nz.add(hp);
-            nz.add(mp);
-            if(!heroes.containsKey(name)) {
-                heroes.put(name, nz);
+            data.add(hp);
+            data.add(mp);
+            if(!heroes.containsKey(name))
+            {
+                heroes.put(name, data);
             }
         }
-        String com=scanner.nextLine();
-        while (!com.equals("End"))
+
+        String commandEnd=scanner.nextLine();
+        while (!commandEnd.equals("End"))
         {
-            String[] input=com.split("\\s\\-\\s");
+            String[] input=commandEnd.split("\\s\\-\\s");
             String command=input[0];
             String name=input[1];
-           switch (command)
-           {
-               case "CastSpell":
-                   int mpneed=Integer.parseInt(input[2]);
-                   String spname=input[3];
-                   int razlika=heroes.get(name).get(1)-mpneed;
-                   if(razlika>=0)
-                   {
-                     int hp=heroes.get(name).get(0);
-                     int mp=heroes.get(name).get(1)-mpneed;
-                     List<Integer> nz=new ArrayList<>();
-                     nz.add(hp);
-                     nz.add(mp);
-                     heroes.put(name , nz);
-                     System.out.println(name+	" has successfully cast "+spname+" and now has "+razlika+" MP!");
-                   }else System.out.println(name+" does not have enough MP to cast "+spname+"!");
-           break;
-               case "TakeDamage":int damage=Integer.parseInt(input[2]);
-               String attacker=input[3];
-                   int razlika1=heroes.get(name).get(0)-damage;
-                  if(razlika1>0)
-                  {
-                      int mp=heroes.get(name).get(1);
-                      List<Integer> nz=new ArrayList<>();
-                      nz.add(razlika1);
-                      nz.add(mp);
-                      heroes.put(name,nz);
-                      System.out.println(name+	" was hit for "+damage+" HP by "+attacker+" and now has "+razlika1+" HP left!");
-                  }else {System.out.println(name+" has been killed by "+attacker+"!");
-                  heroes.remove(name);}
-          break;
-               case "Recharge":
-                   int amount=Integer.parseInt(input[2]);
-                   int mp=heroes.get(name).get(1)+amount;
-                   if(mp>200)
-                   {
-                       int krai=200-heroes.get(name).get(1);
-                       mp=200;
-                       System.out.println(name+	" recharged for "+krai+" MP!");
-                   }else System.out.println(name+	" recharged for "+amount+" MP!");
-                   int hp=heroes.get(name).get(0);
-                   List<Integer> nz=new ArrayList<>();
-                   nz.add(hp);
-                   nz.add(mp);
-                   heroes.put(name, nz);
-                   break;
-               case "Heal":
-                   int amount1=Integer.parseInt(input[2]);
-                   int hp1=heroes.get(name).get(0)+amount1;
-                   if(hp1>100)
-                   {
-                      int krai1=100-heroes.get(name).get(0);
-                       hp1=100;
-                       System.out.println(name+	" healed for "+krai1+" HP!");
-                   }else System.out.println(name+	" healed for "+amount1+" HP!");
-                   int mp1=heroes.get(name).get(1);
-                   List<Integer> nz1=new ArrayList<>();
-                   nz1.add(hp1);
-                   nz1.add(mp1);
-                   heroes.put(name, nz1);
-                   break;
+            switch (command) {
+                case "CastSpell" -> {
+                    int mpneed = Integer.parseInt(input[2]);
+                    String spname = input[3];
+                    int subtraction = heroes.get(name).get(1) - mpneed;
+                    if (subtraction >= 0)
+                    {
+                        int hp = heroes.get(name).get(0);
+                        int mp = heroes.get(name).get(1) - mpneed;
+                        List<Integer> nz = new ArrayList<>();
+                        nz.add(hp);
+                        nz.add(mp);
+                        heroes.put(name, nz);
+                        System.out.println(name + " has successfully cast " + spname + " and now has " + subtraction + " MP!");
+                    }
+                    else
+                    {
+                        System.out.println(name + " does not have enough MP to cast " + spname + "!");
+                    }
+                }
+                case "TakeDamage" -> {
+                    int damage = Integer.parseInt(input[2]);
+                    String attacker = input[3];
+                    int subtraction = heroes.get(name).get(0) - damage;
+                    if (subtraction > 0)
+                    {
+                        int mp = heroes.get(name).get(1);
+                        List<Integer> data = new ArrayList<>();
+                        data.add(subtraction);
+                        data.add(mp);
+                        heroes.put(name, data);
+                        System.out.println(name + " was hit for " + damage + " HP by " + attacker + " and now has " + subtraction + " HP left!");
+                    }
+                    else
+                    {
+                        System.out.println(name + " has been killed by " + attacker + "!");
+                        heroes.remove(name);
+                    }
+                }
+                case "Recharge" -> {
+                    int amount = Integer.parseInt(input[2]);
+                    int mp = heroes.get(name).get(1) + amount;
+                    if (mp > 200)
+                    {
+                        int krai = 200 - heroes.get(name).get(1);
+                        mp = 200;
+                        System.out.println(name + " recharged for " + krai + " MP!");
+                    }
+                    else System.out.println(name + " recharged for " + amount + " MP!");
 
-           }
-
-            com=scanner.nextLine();
+                    int hp = heroes.get(name).get(0);
+                    List<Integer> data = new ArrayList<>();
+                    data.add(hp);
+                    data.add(mp);
+                    heroes.put(name, data);
+                }
+                case "Heal" -> {
+                    int amount = Integer.parseInt(input[2]);
+                    int hp = heroes.get(name).get(0) + amount;
+                    if (hp > 100)
+                    {
+                        int end = 100 - heroes.get(name).get(0);
+                        hp = 100;
+                        System.out.println(name + " healed for " + end + " HP!");
+                    }
+                    else System.out.println(name + " healed for " + amount + " HP!");
+                    int mp = heroes.get(name).get(1);
+                    List<Integer> data = new ArrayList<>();
+                    data.add(hp);
+                    data.add(mp);
+                    heroes.put(name, data);
+                }
+            }
+            commandEnd=scanner.nextLine();
         }
-        heroes.entrySet().stream().sorted((e1,e2)->e2.getValue().get(0).compareTo(e1.getValue().get(0))).forEach(e->System.out.printf("%s%n  HP: %d%n  MP: %d%n", e.getKey(),e.getValue().get(0),e.getValue().get(1)));
+        heroes.entrySet().stream().sorted((e1,e2)->e2.getValue().get(0)
+                .compareTo(e1.getValue().get(0)))
+                .forEach(e->System.out.printf("%s%n  HP: %d%n  MP: %d%n", e.getKey(),e.getValue().get(0),e.getValue().get(1)));
     }
 }
